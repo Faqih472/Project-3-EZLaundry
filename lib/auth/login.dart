87505ec55e5880072bf47f2896b0e56e4dbc23bd
Faqih472 <mfaqih47289@gmail.com>
2025-06-02 19:00:58 +0700

    update login and register page for (bisa register akun user dan login menggunakan otentikasi firebase)

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'home.dart'; // import MyHomePage dari home.dart
-import 'register.dart';
+import '../home.dart'; // import MyHomePage dari home.dart
+import '../register.dart';
 
 class LoginPage extends StatefulWidget {
   @override
